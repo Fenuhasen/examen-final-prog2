6a316a50267e1c34195d2f5da70c143c4fe573f4
Fenohasina RATEFINJANAHARY <hei.fenohasina.5@gmail>
2026-08-20 13:19:58 +0300

    chore: Create database files

---
+++ database/insert.sql
@@ -0,0 +1,14 @@
+INSERT INTO product (id, name, description, unit_price)
+VALUES
+('p001', 'Clavier', 'Clavier mécanique', 75.00),
+('p002', 'Souris', 'Souris sans fil', 35.50),
+('p003', 'Écran', 'Écran 24 pouces', 180.00);
+
+INSERT INTO stock_movement
+    (id, movement_type, quantity, product_id)
+VALUES
+('m001', 'IN', 10, 'p001'),
+('m002', 'OUT', 2, 'p001'),
+('m003', 'IN', 20, 'p002'),
+('m004', 'IN', 5, 'p003');
+
